9241ae52bc367d30a0c7a8acf687a9c7bcc6cee6
John Rudge <jfr23@cam.ac.uk>
2022-03-10 14:35:55 +0000

    Bump version to 0.9.6

--- src/doublespike/__init__.py
+++ src/doublespike/__init__.py
@@ -37,7 +37,7 @@ Classes:
     IsoData                - Object for storing data on individual isotope systems
 """
 
-__version__ = "0.9.5"
+__version__ = "0.9.6"
 __author__ = "John F. Rudge"
 
 from .isodata import IsoData

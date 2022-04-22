078b777075df409d837c71150d2fea32f9ade40d
John Rudge <jfr23@cam.ac.uk>
2022-04-22 17:13:26 +0100

    Broaden inversion test.

--- src/test/test_inversion.py
+++ src/test/test_inversion.py
@@ -1,10 +1,23 @@
 from doublespike import dsinversion, IsoData
+from doublespike.isodata import normalise_composition
+import pytest
 
+test_data = [
+    ("Fe", [0.0, 0.0, 0.5, 0.5], None, 0.5, -0.2, 1.7),
+    ("Ca", [0.031, 0.367, 0.001, 0.002, 0.0, 0.597], [40, 42, 44, 48], 0.3, 0.1, -0.2)
+]
 
-def test_inversion():
-    isodata_fe = IsoData("Fe")
-    isodata_fe.spike = [0.0, 0.0, 0.5, 0.5]
-    measured = [0.2658, 4.4861, 2.6302, 2.6180]
-
-    z = dsinversion(isodata_fe, measured)
-    assert abs(z["alpha"] + 0.1907413) < 1e-7
+@pytest.mark.parametrize("element, spike, isoinv, prop, alpha, beta",test_data)
+def test_inversion(element, spike, isoinv, prop, alpha, beta):
+    isodata = IsoData(element)
+    isodata.spike = spike
+    if isoinv is not None:
+        isodata.isoinv = isoinv
+    sample = normalise_composition(isodata.standard * isodata.mass**(-alpha))
+    mixture = prop * isodata.spike + (1.0 - prop) * sample
+    measured = normalise_composition(mixture * isodata.mass**beta)
+    
+    z = dsinversion(isodata, measured)
+    assert( abs(z["alpha"] - alpha) <  1e-7)
+    assert( abs(z["beta"] - beta) <  1e-7)
+    assert( abs(z["prop"] - prop) <  1e-7)

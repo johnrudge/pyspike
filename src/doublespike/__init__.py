bdb617d190a7ba4ef89f7b1ce29d3ad886008b66
John Rudge <jfr23@cam.ac.uk>
2024-09-10 16:49:32 +0100

    pkg_resources is deprecated in python 3.12

--- src/doublespike/__init__.py
+++ src/doublespike/__init__.py
@@ -37,7 +37,7 @@ Classes:
     IsoData                - Object for storing data on individual isotope systems
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "John F. Rudge"
 
 from .isodata import IsoData
--- src/doublespike/isodata.py
+++ src/doublespike/isodata.py
@@ -1,7 +1,8 @@
 """Module which handles the data on isotope systems."""
+
 import csv
 import numpy as np
-import pkg_resources
+import importlib.resources
 
 # Fundamental constants
 elementarycharge = 1.60217646e-19  # coulombs
@@ -12,51 +13,52 @@ def loadrawdata(filename=None):
     """Read in isotope system datafile and return a python dictionary with data."""
     if filename is None:
         resource_package = __name__
-        resource_path = "/".join(("data", "maininput.csv"))  # Do not use os.path.join()
-        filename = pkg_resources.resource_filename(resource_package, resource_path)
+        resource_path = "/".join(("data", "maininput.csv"))
+        ref = importlib.resources.files(resource_package) / resource_path
+        filename = importlib.resources.as_file(ref)
+    with filename as infile:
+        f = open(infile)
+        csvreader = csv.reader(f)
 
-    f = open(filename)
-    csvreader = csv.reader(f)
+        data = {}
 
-    data = {}
+        next(csvreader)  # ignore header row
+        j = 0
+        els = []
 
-    next(csvreader)  # ignore header row
-    j = 0
-    els = []
+        for row in csvreader:
+            if not len(row[0]) == 0:
+                el = row[0].strip('"')
+                j = j + 1
+                els.append(el)
+                data[el] = {
+                    "element": el,
+                    "isonum": [],
+                    "mass": [],
+                    "standard": [],
+                    "rawspike": [],
+                }
+            data[el]["isonum"].append(int(row[1]))
+            data[el]["mass"].append(float(row[2]))
+            data[el]["standard"].append(float(row[3]))
+            rs = [float(r) for r in row[4:] if len(r) > 0]
+            data[el]["rawspike"].append(rs)
 
-    for row in csvreader:
-        if not len(row[0]) == 0:
-            el = row[0].strip('"')
-            j = j + 1
-            els.append(el)
-            data[el] = {
-                "element": el,
-                "isonum": [],
-                "mass": [],
-                "standard": [],
-                "rawspike": [],
-            }
-        data[el]["isonum"].append(int(row[1]))
-        data[el]["mass"].append(float(row[2]))
-        data[el]["standard"].append(float(row[3]))
-        rs = [float(r) for r in row[4:] if len(r) > 0]
-        data[el]["rawspike"].append(rs)
+        # Renormalise compositions, convert to numpy format
+        for el in els:
+            data[el]["isonum"] = np.array(data[el]["isonum"])
+            data[el]["mass"] = np.array(data[el]["mass"])
 
-    # Renormalise compositions, convert to numpy format
-    for el in els:
-        data[el]["isonum"] = np.array(data[el]["isonum"])
-        data[el]["mass"] = np.array(data[el]["mass"])
+            s = np.array(data[el]["standard"])
+            data[el]["standard"] = s / sum(s)
 
-        s = np.array(data[el]["standard"])
-        data[el]["standard"] = s / sum(s)
+            rs = np.array(data[el]["rawspike"])
+            rs = rs / rs.sum(axis=0)
+            data[el]["rawspike"] = rs.T
+            if len(data[el]["rawspike"]) == 0:
+                data[el]["rawspike"] = None
 
-        rs = np.array(data[el]["rawspike"])
-        rs = rs / rs.sum(axis=0)
-        data[el]["rawspike"] = rs.T
-        if len(data[el]["rawspike"]) == 0:
-            data[el]["rawspike"] = None
-
-    f.close()
+        f.close()
     return data
 
 
